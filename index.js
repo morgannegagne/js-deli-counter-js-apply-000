c1a47cec68e4f20e7d6d17beb42925a35c48addc
Morganne Gagne <morgannegagne@gmail.com>
2017-11-21 12:29:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
+<<<<<<< HEAD
 var katzDeli = []; // create an empty array
 
 function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name) // add name to the end of the Katz Deli line
+=======
+var katzDeli = [];
+
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name)
+>>>>>>> c30afa5059a866ffbe91fe6ce03171dec6177ce7
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
 function nowServing(katzDeliLine){
   if (katzDeliLine.length === 0) return 'There is nobody waiting to be served!'
+<<<<<<< HEAD
   else return `Currently serving ${katzDeliLine.shift()}.` // Use shift() to remove the first person in line. katzDeliLine.shift() returns the first name
 }
 
@@ -19,4 +27,7 @@ function currentLine(katzDeliLine){
     line += `${i+1}. ${katzDeliLine[i]}, `
   }
   return line.slice(0, line.length-2) // remove comma and extra space at the end of var line
+=======
+  else return `Currently serving ${katzDeliLine.shift()}.`
+>>>>>>> c30afa5059a866ffbe91fe6ce03171dec6177ce7
 }
